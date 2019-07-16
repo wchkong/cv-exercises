5360738f3857c399567f59e89e7bb251122dc51a
kongweichang <kongweichang@qq.com>
2019-07-16 23:55:42 +0800

    week2:not complete

---
+++ lesson2/RANSAC.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+#random sample consensus
+def ransacMatching(A, B):
+    # A & B: List of List
+
+    return
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
